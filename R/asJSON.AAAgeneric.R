3ace20333f62f287a8a4cf9431011d1459dea485
Stefan McKinnon Edwards <sme@iysik.com>
2020-04-05 12:29:39 +0200

    Exporting asJSON methods to allow for writing own converters.

--- R/asJSON.AAAgeneric.R
+++ R/asJSON.AAAgeneric.R
@@ -1,3 +1,27 @@
+#' @exportMethod  asJSON
+#' @exportPattern asJSON\..*
+#' @examples
+#' # Create JSON serializing for any class
+#' create.bart <- function() {
+#'   structure("Bart", class="bart")
+#' }
+#'
+#' oldClass("bart")
+#' setMethod("asJSON", signature("bart"),
+#'   function(x, ...) asJSON("'eat my shorts'", auto_unbox=TRUE)
+#' )
+#'
+#' bart <- create.bart()
+#' asJSON(bart)
+#'
+#'
+#' simpsons <- list(
+#'   "homer",
+#'   "marge",
+#'   children=list("lisa",bart)
+#' )
+#' toJSON(simpsons)
+#'
 setGeneric("asJSON", function(x, ...) {
   standardGeneric("asJSON")
 })
